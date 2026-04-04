517be432605d42e9f3ea209e905f1ae332d9490f
LautaroAmd <lautarochacon99@gmail.com>
2026-04-04 17:52:38 -0300

    Subi la carpeta

---
+++ Proyecto/Sumadedosnumeros.js
@@ -0,0 +1,4 @@
+let valor1 = prompt("Ingrese el primer número:");
+let valor2 = prompt("Ingrese el segundo número:");  
+let suma = parseFloat(valor1) + parseFloat(valor2);
+console.log("La suma de los dos números es: " + suma);
